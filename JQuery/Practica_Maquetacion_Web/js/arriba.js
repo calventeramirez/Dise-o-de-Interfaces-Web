ba0e9d3e55aebc885de174ee1cb63d2732d5ae05
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2024-01-27 20:57:16 +0100

    Solucionado el tema del slider

--- JQuery/Practica_Maquetacion_Web/js/arriba.js
+++ JQuery/Practica_Maquetacion_Web/js/arriba.js
@@ -1,19 +1,7 @@
-$(document).ready(function() {
-    // Escuchar el evento scroll de la ventana
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 100) {
-        // Si el desplazamiento es mayor a 100px, mostrar el botón
-        $('.flechaArriba').fadeIn();
-      } else {
-        // Si no, ocultar el botón
-        $('.flechaArriba').fadeOut();
-      }
-    });
-  
-    // Escuchar el clic en el botón para ir hacia arriba
-    $('.flechaArriba').click(function() {
-      // Animación para desplazarse hacia arriba de la página
-      $('html, body').animate({ scrollTop: 0 }, 800);
-      return false;
-    });
-  });
+$(".flechaArriba").click(function(e){
+    e.preventDefault();  // le paso como parámetro el evento y le indico con preventDefault que no realice su función estándar.
+    $('html, body').animate({
+        scrollTop:0  // sube a lo más alto de la página.
+    }, 500);  // 0,5 segs
+    return false;
+});
---
+++ JQuery/Practica_Maquetacion_Web/js/slidder.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.slider-1').bxSlider({
+      mode: 'fade',
+      captions: true
+    });
+  });
